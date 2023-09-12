f5243c7ed3f92e9cc362a6556ef11e96a720bb63
GAraujo21 <gabrielimaaraujo@gmail.com>
2023-09-12 17:24:53 -0300

    feat: add put

--- API-microservico-modulo-pratico/src/main/java/com/example/APImicroservicomodulopratico/Controller/CostCenterController.java
+++ API-microservico-modulo-pratico/src/main/java/com/example/APImicroservicomodulopratico/Controller/CostCenterController.java
@@ -1,12 +1,15 @@
 package com.example.APImicroservicomodulopratico.Controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +38,15 @@ public class CostCenterController {
     public ResponseEntity<List<CostCenter>> listAll(){
         List<CostCenter> costCenters = this.costCenterService.listAll();
         return new ResponseEntity<>(costCenters, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CostCenter> update(@PathVariable UUID id, @RequestBody @Valid CostCenterDTO costCenterDTO){ 
+        CostCenter costCenter = costCenterService.update(id, costCenterDTO);
+        if (costCenter != null){
+            return new ResponseEntity<>(costCenter, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     //Aprofundar MAIS TARDE getByID...
--- API-microservico-modulo-pratico/src/main/java/com/example/APImicroservicomodulopratico/Services/CostCenterService.java
+++ API-microservico-modulo-pratico/src/main/java/com/example/APImicroservicomodulopratico/Services/CostCenterService.java
@@ -1,6 +1,8 @@
 package com.example.APImicroservicomodulopratico.Services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import com.example.APImicroservicomodulopratico.DTO.CostCenterDTO;
 import com.example.APImicroservicomodulopratico.Entities.CostCenter;
 import com.example.APImicroservicomodulopratico.Repository.CostCenterRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -27,5 +31,33 @@ public class CostCenterService {
         List<CostCenter> costCenters = costCenterRepository.findAll();
         return costCenters.stream()
                 .collect(Collectors.toList());
+    }
+
+    public CostCenter getById(UUID id){
+        return this.costCenterRepository.findById(id).orElse(null);
+    }
+
+    public CostCenter update(UUID id, @Valid CostCenterDTO costCenterDTO){
+        Optional<CostCenter> optionalCostCenter = this.costCenterRepository.findById(id);
+
+        if (optionalCostCenter.isPresent()) {
+            CostCenter costCenter = optionalCostCenter.get();
+
+            if (costCenterDTO.getName() != null) {
+                costCenter.setName(costCenterDTO.getName());
+            }
+            return this.costCenterRepository.save(costCenter);
+        }else{
+            throw new EntityNotFoundException("Cost Center not found.");
+        }
+    }
+
+    public boolean delete(UUID id){
+        CostCenter costCenter = this.costCenterRepository.findById(id).orElse(null);
+        if (costCenter != null) {
+            this.costCenterRepository.delete(costCenter);
+            return true;
+        }
+        return false;
     }
 }
