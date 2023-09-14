a8ba5970e5df330041a09fc3a6f944d41a658d79
GAraujo21 <gabrielimaaraujo@gmail.com>
2023-09-14 17:42:19 -0300

    feat: create new features

--- API-microservico-modulo-pratico/src/main/java/com/example/APImicroservicomodulopratico/Repository/UserRepository.java
+++ API-microservico-modulo-pratico/src/main/java/com/example/APImicroservicomodulopratico/Repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.APImicroservicomodulopratico.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,5 @@ import com.example.APImicroservicomodulopratico.Entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID>{
-    
+    Optional<User> findByRegistrationNumber(String registrationNumber);
 }
